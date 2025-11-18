2fb78e3af048a3f20ca86a5a023eb74895c35138
Jack <donahuej5@wit.edu>
2025-11-18 15:08:47 -0500

    added questions

--- WHR.py
+++ WHR.py
@@ -6,6 +6,12 @@ import numpy as np
 # WHR Project Data Sci
 # Jack Donahue
 
+# Questions
+# 1) What impact does GDP have on the general happiness level?
+# 2) Does the country's main industries meaningfully impact happiness?
+# 3) What is the impact of government style on Happiness?
+# 4) What (if any) is the impact of air quality on happiness?
+
 filepath = "WHR.csv"
 for airport in destinations:
     delaylist = []
